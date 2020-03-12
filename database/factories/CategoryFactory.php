961683ec72ce1d8cd6194882bf2f30654d489696
unknown <bihansaif09@gmail.com>
2020-03-12 17:55:21 +0600

    Factory and Seed

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -6,7 +6,9 @@ use App\Model\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $word= $faker->sentence;
     return [
-        //
+        'name'=>$word,
+        'slug'=>Str::slug($word),
     ];
 });
--- database/factories/LikeFactory.php
+++ database/factories/LikeFactory.php
@@ -3,10 +3,15 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\Like;
+use App\Model\Reply;
 use Faker\Generator as Faker;
 
 $factory->define(Like::class, function (Faker $faker) {
     return [
-        //
+        'user_id'=> function(){
+         return \App\User::all()->random();},
+          'reply_id'=>function(){
+              return Reply::all()->random();
+          },
     ];
 });
